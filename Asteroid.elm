909b2ba1b71c5f2c5e85967b50f0f59f058b09d4
Simon Hampton <hotbelgo@gmail.com>
2015-09-11 21:03:27 +0200

    asteroids break into 2

--- Asteroid.elm
+++ Asteroid.elm
@@ -1,4 +1,4 @@
-module Asteroid (init, view, update, checkCrash, Model) where
+module Asteroid (init, view, update, hit, miss, filterHits, Model) where
 
 import Color exposing (..)
 import Graphics.Collage exposing (..)
@@ -12,19 +12,19 @@ type alias Model =
     , size : Float
     }
 
-init : Model
-init =  { thing =
-            { pos = { x = 125, y = 50}
-            , direction = 6.0
-            , speed = 1.5
-            }
-        , size = 20
+init : Float -> Float -> Float -> Float -> Float -> Model
+init xs ys d s sz =
+    { thing =
+        { pos = { x = xs, y = ys}
+        , direction = d
+        , speed = s
         }
+    , size = sz
+    }
 
 -- UPDATE
 update : Model -> Model
 update model =
-    -- { model | thing | pos <- Thing.move model }
     let
         t1 = model.thing
         -- Debug.watch "Direciton" (bounce model)
@@ -32,6 +32,13 @@ update model =
         t3 = { t2 | pos <- Thing.move t2 }
         -- newthing = { model.thing | pos <- Thing.move model.thing }
     in { model | thing <- t3 }
+    -- { model |
+    --     thing <-
+    --         { model.thing |
+    --             direction <- bounce model
+    --           , pos <- Thing.move model.thing
+    --         }
+    --     }
 
 bounce : Model -> Float
 bounce model =
@@ -40,6 +47,22 @@ bounce model =
        | abs model.thing.pos.y > (150 - model.size) ->
             floatMod (2 * pi - model.thing.direction) (2 * pi)
        | otherwise -> model.thing.direction
+
+-- concatMap : (a -> List b) -> List a -> List b
+filterHits : List Thing.Model -> List Model -> List Model
+filterHits bullets asteroids =
+    -- List.filter (missAll bullets) asteroids
+    List.concatMap (smash bullets) asteroids
+
+smash : List Thing.Model -> Model -> List Model
+smash bullets a =
+    if missAll bullets a
+        then [a]
+        else if a.size > 10
+            then
+                [ init a.thing.pos.x a.thing.pos.y (a.thing.direction + pi /2) a.thing.speed (a.size / 2)
+                , init a.thing.pos.x a.thing.pos.y (a.thing.direction - pi /2) a.thing.speed (a.size / 2) ]
+            else []
 
 -- VIEW
 
@@ -50,5 +73,12 @@ view model =
         |> move (model.thing.pos.x, model.thing.pos.y)
 
 -- HELPERS
-checkCrash : Thing.Model -> Model -> Bool
-checkCrash thingModel astModel = dist thingModel.pos astModel.thing.pos < astModel.size ^ 2
+hit : Thing.Model -> Model -> Bool
+hit thingModel astModel = dist thingModel.pos astModel.thing.pos < astModel.size ^ 2
+
+miss : Thing.Model -> Model -> Bool
+miss t a = not <| hit t a
+
+-- all Bullets miss this Asteroid
+missAll : List Thing.Model -> Model -> Bool
+missAll things model = List.all (\t -> miss t model) things
--- Game.elm
+++ Game.elm
@@ -10,7 +10,7 @@ import Signal exposing (..)
 import Thing exposing (Keys, Point, dist)
 import Rocket
 import Asteroid
-import Bullet
+import Bullets
 
 -- MODEL
 type Game = Playing | Ended
@@ -19,29 +19,45 @@ type alias Model =
     { state: Game
     , rocket: Thing.Model
     , asteroids : List Asteroid.Model
-    , bullets : List Bullet.Model
+    , bullets : Bullets.Model
     }
 
 
 init =
     { state = Playing
     , rocket = Rocket.init
-    , asteroids = [Asteroid.init]
-    , bullets = []
+    , asteroids = [Asteroid.init 125 50 6 1.5 20]
+    , bullets = { bullets = [], lastBullet = 0 }
     }
 
 -- UPDATE
 
-update : Keys -> Model -> Model
-update ks model =
+update : Input -> Model -> Model
+update (Input t sp ks) model =
+-- update : Keys -> Model -> Model
+-- update ks model =
     if model.state == Playing
     then
         let
-            newModel = moveThings ks model
-            -- filter shot Meteorites
-        in case List.any (Asteroid.checkCrash newModel.rocket) newModel.asteroids of
+            movedThings = moveThings ks model
+            newBullets =
+                -- if <space> then add bullet
+                -- filter bullets that hit an asteroid
+                Bullets.filterHits movedThings.asteroids <|
+                    if sp && (t - movedThings.bullets.lastBullet > 1000)
+                    then Bullets.addBullet movedThings.rocket t movedThings.bullets
+                    else movedThings.bullets
+
+            newAsteroids =
+                Asteroid.filterHits movedThings.bullets.bullets movedThings.asteroids
+
+        in case List.any (Asteroid.hit movedThings.rocket) movedThings.asteroids of
             True -> { model | state <- Ended }
-            False -> newModel
+            False ->
+                { movedThings |
+                      bullets <- newBullets
+                    , asteroids <- newAsteroids
+                }
     else model
 
 moveThings : Keys -> Model -> Model
@@ -49,6 +65,7 @@ moveThings ks model =
     { model |
       rocket <- Rocket.update ks model.rocket
     , asteroids <- List.map Asteroid.update model.asteroids
+    , bullets <- Bullets.update model.bullets
     }
 
 -- checkCrash : Model -> Bool
@@ -64,39 +81,39 @@ view model =
                 |> filled black)
             :: Rocket.view model.rocket
             :: List.map Asteroid.view model.asteroids
-            ++ List.map Bullet.view model.bullets
+            ++ Bullets.view model.bullets
     else
         collage 300 300 <|
             [rect 300 300
                 |> filled black]
 
-viewDebug : Model -> Element
-viewDebug model =
+viewDebug : Model -> Input -> Element
+viewDebug model (Input t sp p) =
     flow right <|
         view model
         -- :: List.map (\m -> show m.thing.pos.x) model.asteroids
-        :: [show <| List.map (\a -> dist model.rocket.pos a.thing.pos) model.asteroids ]
+        :: [show <| List.length model.bullets.bullets ]
 
 -- INPUT
 -- arrows, wasd     :: Signal { x :: Int, y :: Int }
 -- shift,ctrl,space :: Signal Bool
 type Input
-    = Keys {x: Int, y:Int}
-    | Space Time Bool
+    = Input Time Bool {x: Int, y:Int}
 
 input : Signal Input
 input =
     let
-        delta = Signal.map (\t -> t/20) (fps 30)
+        delta = Time.every 100
     in
-        Signal.sampleOn delta (Signal.mergeMany [Signal.map Keys Keyboard.arrows, Signal.map2 Space delta Keyboard.space])
+        Signal.map3 Input delta Keyboard.space Keyboard.arrows
 
 -- MAIN
 main : Signal Element
 main =
     let
-        gameplay = fps 10
-        signals = Signal.sampleOn gameplay Keyboard.arrows
-        model = foldp update init signals
-    in Signal.map viewDebug model
+        -- gameplay = fps 10
+        -- signals = Signal.sampleOn gameplay Keyboard.arrows
+        -- model = foldp update init signals
+        model = foldp update init input
+    in Signal.map2 viewDebug model input
     -- in Signal.map view model
